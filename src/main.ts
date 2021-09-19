3b9b014d1bcfac37cb74bf522433bf71273d7915
Darrell Maples <drmaples@gmail.com>
2021-09-19 16:40:06 -0500

    Initial work (#6)
    * add asdf tools version file
    
    * wire up checking for jira tix in title and body of pr

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,71 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { GitHub } from '@actions/github/lib/utils'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.debug(JSON.stringify(github.context))
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const token = core.getInput('token', { required: true })
+    const quiet = core.getInput('quiet', { required: false }) === 'true'
+    const titleRegexInput = core.getInput('titleRegex', { required: false }) || '^[([A-Z]{2,})(-)(d+)]'
+    const bodyRegexInput = core.getInput('bodyRegex', { required: false }) || '[([A-Z]{2,})(-)(d+)]'
+    const noTicketInput = core.getInput('noTicket', { required: false }) || '[no-ticket]'
 
-    core.setOutput('time', new Date().toTimeString())
+    const client = github.getOctokit(token)
+    const prTitle: string = github.context?.payload?.pull_request?.title
+    const prBody = github.context?.payload?.pull_request?.body
+
+    if (prBody === undefined) {
+      core.debug('pr body is undefined')
+      core.setFailed('Could not retrieve the Pull Request body')
+      return
+    }
+
+    const reTitle = new RegExp(titleRegexInput, 'g')
+    const reBody = new RegExp(bodyRegexInput, 'g')
+    const foundTixInTitle = reTitle.test(prTitle)
+    const foundTixInBody = reBody.test(prBody)
+    const foundNoTixInTitle = prTitle.startsWith(noTicketInput)
+    const foundNoTixInBody = prBody.includes(noTicketInput)
+    core.debug(`found tix in title: ${foundTixInTitle}`)
+    core.debug(`found tix in body : ${foundTixInBody}`)
+    core.debug(`found no-tix in title: ${foundNoTixInTitle}`)
+    core.debug(`found no-tix in body : ${foundNoTixInBody}`)
+
+    if (foundTixInTitle && foundTixInBody) {
+      core.debug('success, found tix in both title and body')
+      return
+    }
+    if (foundNoTixInTitle && foundNoTixInBody) {
+      core.debug(`success, found ${noTicketInput} in both title and body`)
+      return
+    }
+
+    core.setFailed('missing JIRA ticket in both PR title AND body')
+    makePrComment(client, quiet)
   } catch (error) {
     core.setFailed(error.message)
+  }
+}
+
+async function makePrComment(client: InstanceType<typeof GitHub>, quiet: boolean): Promise<void> {
+  try {
+    if (quiet) {
+      core.debug(`quiet mode enabled. skipping creating PR comment.`)
+      return
+    }
+
+    const { owner, repo, number } = github.context.issue
+    await client.rest.pulls.createReview({
+      owner,
+      repo,
+      pull_number: number,
+      body: 'PR title AND body does not contain a reference to a JIRA ticket.',
+      event: 'COMMENT'
+    })
+  } catch (error) {
+    core.setFailed(`Failed to update PR`)
   }
 }
 
