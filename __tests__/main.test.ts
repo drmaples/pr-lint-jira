f946f5fe56750ebf26f0e2ae4ebfbe5ee67b92cc
Darrell Maples <drmaples@gmail.com>
2021-09-19 19:37:10 -0500

    fix regex, add a test (#8)
    * fix regex, add a test
    
    * fix tests
    
    * more fixes
    
    - setup action.yml
    - more logging
    
    * oops, fix yaml indentation
    
    * detect CI mode, rename input param

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,5 +1,23 @@
 import { expect, test } from '@jest/globals'
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
 
-test('junk', () => {
-  expect(1).toEqual(1)
+test('test regex', () => {
+  const re = new RegExp(`\\[([A-Z]{2,}-\\d{3,})\\]`, 'g')
+  expect(re.test('foo')).toEqual(false)
+  expect(re.test('FOO-123')).toEqual(false)
+  expect(re.test('[FOO-123]')).toEqual(true)
+})
+
+test('test runs', () => {
+  process.env['IS_CI'] = 'true'
+  process.env['INPUT_TOKEN'] = 'xxxxxx'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  try {
+    cp.execSync(`node ${ip}`, { env: process.env })
+  } catch (error) {
+    console.log(error.message)
+    console.log(error.stdout.toString())
+  }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
+/* eslint no-console: */
+
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as process from 'process'
 import { GitHub } from '@actions/github/lib/utils'
 
 async function run(): Promise<void> {
   try {
     core.debug(JSON.stringify(github.context))
 
+    const isCI = process.env.IS_CI === 'true'
     const token = core.getInput('token', { required: true })
-    const quiet = core.getInput('quiet', { required: false }) === 'true'
-    const titleRegexInput = core.getInput('titleRegex', { required: false }) || `^\[([A-Z]{2,})(-)(\d+)\]` //eslint-disable-line no-useless-escape
-    const bodyRegexInput = core.getInput('bodyRegex', { required: false }) || `\[([A-Z]{2,})(-)(\d+)\]` //eslint-disable-line no-useless-escape
+    const makePrComment = core.getInput('makePrComment', { required: false }) === 'true'
+    const titleRegexInput = core.getInput('titleRegex', { required: false }) || `^\\[([A-Z]{2,}-\\d{3,})\\]`
+    const bodyRegexInput = core.getInput('bodyRegex', { required: false }) || `\\[([A-Z]{2,}-\\d{3,})\\]`
     const noTicketInput = core.getInput('noTicket', { required: false }) || '[no-ticket]'
 
     const client = github.getOctokit(token)
-    const prTitle: string = github.context?.payload?.pull_request?.title
-    const prBody = github.context?.payload?.pull_request?.body
-
-    if (prBody === undefined) {
-      core.debug('pr body is undefined')
-      core.setFailed('Could not retrieve the Pull Request body')
-      return
-    }
+    const prTitle: string = github.context?.payload?.pull_request?.title || ''
+    const prBody = github.context?.payload?.pull_request?.body || ''
 
     const reTitle = new RegExp(titleRegexInput, 'g')
     const reBody = new RegExp(bodyRegexInput, 'g')
@@ -28,34 +26,35 @@ async function run(): Promise<void> {
     const foundTixInBody = reBody.test(prBody)
     const foundNoTixInTitle = prTitle.startsWith(noTicketInput)
     const foundNoTixInBody = prBody.includes(noTicketInput)
-    core.debug(`found tix in title: ${foundTixInTitle}`)
-    core.debug(`found tix in body : ${foundTixInBody}`)
-    core.debug(`found no-tix in title: ${foundNoTixInTitle}`)
-    core.debug(`found no-tix in body : ${foundNoTixInBody}`)
+    core.info(`found tix in title: ${foundTixInTitle}`)
+    core.info(`found tix in body : ${foundTixInBody}`)
+    core.info(`found no-tix in title: ${foundNoTixInTitle}`)
+    core.info(`found no-tix in body : ${foundNoTixInBody}`)
 
     if (foundTixInTitle && foundTixInBody) {
-      core.debug('success, found tix in both title and body')
+      core.info('success, found tix in both title and body')
       return
     }
     if (foundNoTixInTitle && foundNoTixInBody) {
-      core.debug(`success, found ${noTicketInput} in both title and body`)
+      core.info(`success, found ${noTicketInput} in both title and body`)
+      return
+    }
+    if (isCI) {
+      core.info(`ci mode detected. not returning failure`)
       return
     }
 
     core.setFailed('missing JIRA ticket in both PR title AND body')
-    makePrComment(client, quiet)
+    if (makePrComment === true) {
+      createPRComment(client)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-async function makePrComment(client: InstanceType<typeof GitHub>, quiet: boolean): Promise<void> {
+async function createPRComment(client: InstanceType<typeof GitHub>): Promise<void> {
   try {
-    if (quiet) {
-      core.debug(`quiet mode enabled. skipping creating PR comment.`)
-      return
-    }
-
     const { owner, repo, number } = github.context.issue
     await client.rest.pulls.createReview({
       owner,
