49fbabe87ac088708490278c9e569721a7d1253c
drmaples <darrell@grocerytv.com>
2026-01-03 14:23:09 -0600

    move some stuff around

--- app/internal/github/client.go
+++ app/internal/github/client.go
@@ -1,9 +1,7 @@
 package github
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 )
 
 type Iface interface {
@@ -23,25 +21,7 @@ func NewGithub(token string) Iface {
 }
 
 func (a *api) Context() (*GHContext, error) {
-	gh := &GHContext{
-		EventPath: os.Getenv("GITHUB_EVENT_PATH"),
-		EventName: os.Getenv("GITHUB_EVENT_NAME"),
-		SHA:       os.Getenv("GITHUB_SHA"),
-		Ref:       os.Getenv("GITHUB_REF"),
-	}
-
-	if gh.EventPath != "" {
-		content, err := os.ReadFile(gh.EventPath)
-		if err != nil {
-			return nil, fmt.Errorf("problem reading github event path: %w", err)
-		}
-
-		if err := json.Unmarshal(content, &gh.Payload); err != nil {
-			return nil, fmt.Errorf("problem with payload unmarshal: %w", err)
-		}
-	}
-
-	return gh, nil
+	return newGHContext()
 }
 
 func (a *api) CreateComment(comment string) error {
--- app/internal/github/context.go
+++ app/internal/github/context.go
@@ -1,21 +1,30 @@
 package github
 
-// modeled after https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// inspired by https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts
 
 type PullRequest struct {
-	Title string `json:"title"`
-	Body  string `json:"body"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	Number int    `json:"number"`
 }
 
 type Issue struct {
 	Owner  string `json:"owner"`
 	Repo   string `json:"repo"`
-	Number string `json:"number"` // issue number is same as PR number
+	Number int    `json:"number"`
 }
 
 type Payload struct {
 	PullRequest PullRequest `json:"pull_request"`
 	Issue       Issue       `json:"issue"`
+	Number      int         `json:"number"`
 }
 
 type GHContext struct {
@@ -24,4 +33,48 @@ type GHContext struct {
 	EventPath string
 	SHA       string
 	Ref       string
+}
+
+func newGHContext() (*GHContext, error) {
+	c := &GHContext{
+		EventPath: os.Getenv("GITHUB_EVENT_PATH"),
+		EventName: os.Getenv("GITHUB_EVENT_NAME"),
+		SHA:       os.Getenv("GITHUB_SHA"),
+		Ref:       os.Getenv("GITHUB_REF"),
+	}
+
+	if c.EventPath != "" {
+		c.loadPayload()
+	}
+	return c, nil
+}
+
+func (c *GHContext) loadPayload() error {
+	content, err := os.ReadFile(c.EventPath)
+	if err != nil {
+		return fmt.Errorf("problem reading github event path: %w", err)
+	}
+
+	if err := json.Unmarshal(content, &c.Payload); err != nil {
+		return fmt.Errorf("problem with payload unmarshal: %w", err)
+	}
+	return nil
+}
+
+func (c *GHContext) Issue() (string, string, int) {
+	owner, repo := c.Repo()
+
+	number := 0
+	if c.Payload.Issue.Number > 0 {
+		number = c.Payload.Issue.Number
+	} else if c.Payload.PullRequest.Number > 0 {
+		number = c.Payload.PullRequest.Number
+	}
+	return owner, repo, number
+}
+
+func (c *GHContext) Repo() (string, string) {
+	full := os.Getenv("GITHUB_REPOSITORY")
+	nuggets := strings.SplitN(full, "/", 2)
+	return nuggets[0], nuggets[1]
 }
